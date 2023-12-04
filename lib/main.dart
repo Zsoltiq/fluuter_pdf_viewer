893c167fb71359f88d6da5699314b5228f0cc26c
diak <diak@mesterlevel.lo>
2023-12-04 10:56:56 +0100

    elso

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MainApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MainApp extends StatelessWidget {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Syncfusion Flutter PdfViewer'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.bookmark,
+                color: Colors.white,
+                semanticLabel: 'Bookmark',
+              ),
+              onPressed: () {
+                _pdfViewerKey.currentState?.openBookmarkView();
+              },
+            ),
+          ],
+        ),
+        body: SfPdfViewer.network(
+          'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+          key: _pdfViewerKey,
+        ),
+      ),
+    );
+  }
+}
